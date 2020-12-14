92f2cd55cb5d78a473171726f0c1cd0b6981a9ac
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2020-12-14 01:09:46 -0500

    Update magento_2_ce_2.3.sh

--- magento_2_ce_2.3.sh
+++ magento_2_ce_2.3.sh
@@ -2,34 +2,21 @@ cd /var/www/html/
 
 echo "***************************************************************************************************"
 echo ""
-echo ""
-echo -n -e "\e[1;32mPlease give a me a Version Number to download the Magento 2 CE in the root directory\e[0m"
-echo ""
+echo -n -e "\e[1;32mMagento Info                  :  --\e[0m"
 echo ""
 echo "***************************************************************************************************"
 echo ""
-echo ""
-echo -n -e "\e[1;33mMagento2 Public Key:  -- \e[0m ";   read MAGENTO_PUBLIC_KEY
-echo -n -e "\e[1;33mMagento2 Private Key:  -- \e[0m ";  read MAGENTO_PRIVATE_KEY
-echo ""
-echo ""
-echo "***************************************************************************************************"
-echo ""
-echo ""
-echo -n -e "\e[1;33mMagento2 Edition:  -- \e[0m ";      read MA_EDI
-echo ""
+echo -n -e "\e[1;33mMagento2 Public Key           :  -- \e[0m ";  read MAGENTO_PUBLIC_KEY
+echo -n -e "\e[1;33mMagento2 Private Key          :  -- \e[0m ";  read MAGENTO_PRIVATE_KEY
+echo -n -e "\e[1;33mMagento2 Edition              :  -- \e[0m ";  read MA_EDI
 echo ""
 echo "***************************************************************************************************"
 echo ""
-echo ""
-echo -e "\e[1;32mPlease give a me a folder name to download the Magento 2 CE in the root directory  \e[0m"
-echo ""
+echo -n -e "\e[1;32mPlease give a me a folder name to download the Magento 2 CE in the root directory  \e[0m"
 echo ""
 echo "***************************************************************************************************"
 echo ""
-echo ""
-echo -n -e "\e[1;33mFolder name:  -- \e[0m ";          read DIR_NAME
-echo ""
+echo -n -e "\e[1;33mFolder name                   :  -- \e[0m ";   read DIR_NAME
 echo ""
 
 composer config -g http-basic.repo.magento.com $MAGENTO_PUBLIC_KEY $MAGENTO_PRIVATE_KEY
@@ -48,16 +35,11 @@ systemctl enable apache2.service
 chmod 777 -R /var/
 cd /var/www/html/$DIR_NAME/
 
-echo ""
-echo ""
 echo "***************************************************************************************************"
-echo ""
 echo ""
 echo -n -e "\e[1;32mAfter complete the installation, \e[1;33mPlease press Enter \e[0m"
 echo ""
-echo ""
 echo "***************************************************************************************************"
-echo ""
 echo ""
 read Enter
 
