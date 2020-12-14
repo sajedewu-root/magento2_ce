b146999e53a7778712ebfb76a4b976613e4f4155
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2020-12-14 01:14:50 -0500

    Update magento_2_ce_2.4.sh

--- magento_2_ce_2.4.sh
+++ magento_2_ce_2.4.sh
@@ -4,35 +4,21 @@ cd /var/www/html/
 
 echo "***************************************************************************************************"
 echo ""
-echo ""
-echo -n -e "\e[1;32mPlease give a me a Version Number to download the Magento 2 CE in the root directory\e[0m"
-echo ""
+echo -n -e "\e[1;32mMagento Info                  :  --\e[0m"
 echo ""
 echo "***************************************************************************************************"
 echo ""
-echo ""
-echo -n -e "\e[1;33mMagento2 Public Key:  -- \e[0m ";   read MAGENTO_PUBLIC_KEY
-echo ""
-echo -n -e "\e[1;33mMagento2 Private Key:  -- \e[0m ";  read MAGENTO_PRIVATE_KEY
-echo ""
+echo -n -e "\e[1;33mMagento2 Public Key           :  -- \e[0m ";  read MAGENTO_PUBLIC_KEY
+echo -n -e "\e[1;33mMagento2 Private Key          :  -- \e[0m ";  read MAGENTO_PRIVATE_KEY
+echo -n -e "\e[1;33mMagento2 Edition              :  -- \e[0m ";  read MA_EDI
 echo ""
 echo "***************************************************************************************************"
 echo ""
-echo ""
-echo -n -e "\e[1;33mMagento2 Edition:  -- \e[0m ";      read MA_EDI
-echo ""
+echo -n -e "\e[1;32mPlease give a me a folder name to download the Magento 2 CE in the root directory  \e[0m"
 echo ""
 echo "***************************************************************************************************"
 echo ""
-echo ""
-echo -e "\e[1;32mPlease give a me a folder name to download the Magento 2 CE in the root directory  \e[0m"
-echo ""
-echo ""
-echo "***************************************************************************************************"
-echo ""
-echo ""
-echo -n -e "\e[1;33mFolder name:  -- \e[0m ";          read DIR_NAME
-echo ""
+echo -n -e "\e[1;33mFolder name                   :  -- \e[0m ";  read DIR_NAME
 echo ""
 
 composer config -g http-basic.repo.magento.com $MAGENTO_PUBLIC_KEY $MAGENTO_PRIVATE_KEY
@@ -57,21 +43,19 @@ echo -n -e "Magento 2 Command Line installation process   : -- \e[0m "
 echo ""
 echo "************************************************************************"
 echo ""
-echo ""
-echo -n -e "\e[1;33mMagento URL                  : -- \e[0m ";  read URL
-echo -n -e "\e[1;33mDatabase Host Name           : -- \e[0m ";  read DB_HOST_NAME
-echo -n -e "\e[1;33mDatabase Name                : -- \e[0m ";  read DB_NAME 
-echo -n -e "\e[1;33mDatabase User                : -- \e[0m ";  read DB_USER_ID
-echo -n -e "\e[1;33mDatabse Passowrd             : -- \e[0m ";  read DB_PASSWORD
-echo -n -e "\e[1;33mAdministrator First Name     : -- \e[0m ";  read DB_ADMIN_FNAME
-echo -n -e "\e[1;33mAdministrator Last Name      : -- \e[0m ";  read DB_ADMIN_LNAME
-echo -n -e "\e[1;33mAdministrator Emaill Address : -- \e[0m ";  read DB_ADMIN_EMAIL
-echo -n -e "\e[1;33mAdministrator User Id        : -- \e[0m ";  read DB_ADMIN_ID
-echo -n -e "\e[1;33mAdministrator User Password  : -- \e[0m ";  read DB_ADMIN_PASSWORD
-echo -n -e "\e[1;33mLanguage                     : -- \e[0m ";  read LANG
-echo -n -e "\e[1;33mCurrency Ex. USD             : -- \e[0m ";  read CURR
-echo -n -e "\e[1;33mTimezone Ex. America/Chicago : -- \e[0m ";  read TIME_ZONE
-echo ""
+echo -n -e "\e[1;33mMagento URL                   :  -- \e[0m ";  read URL
+echo -n -e "\e[1;33mDatabase Host Name            :  -- \e[0m ";  read DB_HOST_NAME
+echo -n -e "\e[1;33mDatabase Name                 :  -- \e[0m ";  read DB_NAME 
+echo -n -e "\e[1;33mDatabase User                 :  -- \e[0m ";  read DB_USER_ID
+echo -n -e "\e[1;33mDatabse Passowrd              :  -- \e[0m ";  read DB_PASSWORD
+echo -n -e "\e[1;33mAdministrator First Name      :  -- \e[0m ";  read DB_ADMIN_FNAME
+echo -n -e "\e[1;33mAdministrator Last Name       :  -- \e[0m ";  read DB_ADMIN_LNAME
+echo -n -e "\e[1;33mAdministrator Emaill Address  :  -- \e[0m ";  read DB_ADMIN_EMAIL
+echo -n -e "\e[1;33mAdministrator User Id         :  -- \e[0m ";  read DB_ADMIN_ID
+echo -n -e "\e[1;33mAdministrator User Password   :  -- \e[0m ";  read DB_ADMIN_PASSWORD
+echo -n -e "\e[1;33mLanguage                      :  -- \e[0m ";  read LANG
+echo -n -e "\e[1;33mCurrency Ex. USD              :  -- \e[0m ";  read CURR
+echo -n -e "\e[1;33mTimezone Ex. America/Chicago  :  -- \e[0m ";  read TIME_ZONE
 echo ""
 
 
@@ -90,18 +74,11 @@ bin/magento setup:install --base-url=http://$URL/$DIR_NAME/ \
 --timezone=$TIME_ZONE \
 --use-rewrites=1
 
-
-echo ""
-echo ""
 echo "***************************************************************************************************"
-echo ""
 echo ""
 echo -n -e "\e[1;32mAfter complete the installation, \e[1;33mPlease press Enter \e[0m"
 echo ""
-echo ""
 echo "***************************************************************************************************"
-echo ""
-echo ""
 read Enter
 
 cd /var/www/html/$DIR_NAME/
