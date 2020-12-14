10f544ebe2a0645294467b8b634e1b2dfc95dab6
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2020-12-14 01:01:53 -0500

    Update and rename magento_2_ce.sh to magento_2_ce_2.4.sh

--- magento_2_ce_2.4.sh
+++ magento_2_ce_2.4.sh
@@ -109,6 +109,7 @@ php bin/magento indexer:reindex
 php bin/magento cron:install
 # composer update
 # php bin/magento setup:upgrade
+php bin/magento module:disable Magento_TwoFactorAuth
 
 chmod -R 777 /var/
 clear
