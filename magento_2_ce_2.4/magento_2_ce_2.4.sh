f8e288aadded932d775849502556534501a0d520
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2022-04-06 00:03:16 -0400

    Update magento_2_ce_2.4.sh
    Composer 2 Install added

--- magento_2_ce_2.4/magento_2_ce_2.4.sh
+++ magento_2_ce_2.4/magento_2_ce_2.4.sh
@@ -21,8 +21,18 @@ echo ""
 echo -n -e "\e[1;33mFolder name                   :  -- \e[0m ";  read DIR_NAME
 echo ""
 
-composer config -g http-basic.repo.magento.com $MAGENTO_PUBLIC_KEY $MAGENTO_PRIVATE_KEY
-composer create-project --repository=https://repo.magento.com/ magento/project-community-edition=$MA_EDI $DIR_NAME
+sudo apt update -y
+sudo apt upgrade -y
+
+cd ~
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer
+
+# composer config -g http-basic.repo.magento.com $MAGENTO_PUBLIC_KEY $MAGENTO_PRIVATE_KEY
+# composer create-project --repository=https://repo.magento.com/ magento/project-community-edition=$MA_EDI $DIR_NAME
 
 chmod -R 777 /var/
 chown -R www-data:www-data /var/www/html/
